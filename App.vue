47bdafcc0163b7099a0e8801a2985b68ea38f9d8
liuyan-lucky <45897078+liuyan-lucky@users.noreply.github.com>
2018-12-16 15:36:30 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="dell" @click="handleClick">{{message}}</div>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      message:"hello"
+    }
+  },
+  methods:{
+    handleClick(){
+      this.message="bye"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dell{
+  color:red;
+}
+</style>
